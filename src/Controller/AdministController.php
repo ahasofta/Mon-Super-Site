72c30079dead4a009e831998666f0c1b4195127e
IBRAHIM AHAMADA M'SA <ibrahimahamada001a@gmail.com>
2020-04-24 02:30:38 +0200

    suppression des migration

--- src/Controller/AdministController.php
+++ src/Controller/AdministController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Medecin;
+use App\Form\MedecinType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdministController extends Controller
 {
     /**
-     * @Route("/administ", name="administ")
+     * @Route("/admin", name="admin")
      */
     public function index()
     {
         return $this->render('administ/index.html.twig', [
             'controller_name' => 'AdministController',
+        ]);
+    }
+    /**
+     * @Route("/admin/new/medecin", name="create_medecin")
+     */
+    public function ActionMedcin (){
+        $medecin = new Medecin();
+        $form = $this->createForm(MedecinType::class, $medecin);
+
+        return $this->render("medecin/action.html.twig",[
+            'formMedecin' => $form->createView()
         ]);
     }
     
