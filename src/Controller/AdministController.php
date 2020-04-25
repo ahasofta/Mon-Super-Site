ff5ca654aa1edaa135f63e83f201afdd7316b165
IBRAHIM AHAMADA M'SA <ibrahimahamada001a@gmail.com>
2020-04-25 04:27:24 +0200

    page hom

--- src/Controller/AdministController.php
+++ src/Controller/AdministController.php
@@ -9,9 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class AdministController extends Controller
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_home")
      */
-    public function index()
+    public function home()
     {
         return $this->render('administ/index.html.twig', [
             'controller_name' => 'AdministController',
