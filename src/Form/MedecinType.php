e8e281b25b83abf76201030e791c8dd71b80834d
AHAMADI YOUSSOUF <ahasofta@gmail.com>
2020-04-26 21:38:49 +0300

    suppression dans medecinType

--- src/Form/MedecinType.php
+++ src/Form/MedecinType.php
@@ -5,10 +5,6 @@ namespace App\Form;
 use App\Entity\Medecin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-<<<<<<< HEAD
-=======
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
->>>>>>> ff5ca654aa1edaa135f63e83f201afdd7316b165
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MedecinType extends AbstractType
@@ -20,12 +16,7 @@ class MedecinType extends AbstractType
             ->add('prenom_med')
             ->add('specialite')
             ->add('localisation')
-<<<<<<< HEAD
-            ->add('visualiser')
-            ->add('patients')
-=======
-            
->>>>>>> ff5ca654aa1edaa135f63e83f201afdd7316b165
+
         ;
     }
 
